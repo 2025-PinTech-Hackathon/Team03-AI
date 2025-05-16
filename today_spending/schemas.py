c357b7ebfed507f19ca618264c0cf51c3227a44c
Haemin Kim <haemin1230@gmail.com>
2025-05-16 21:09:23 +0900

    📦 (#1) 기본 파일 구조 생성

---
+++ today_spending/schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from typing import List
+from datetime import datetime
+
+class Transaction(BaseModel):
+    merchant_name: str
+    amount: int
+    timestamp: datetime
+
+class SpendingRequest(BaseModel):
+    userId: int
+    role: str
+    transactions: List[Transaction]
+
+class ReviewResponse(BaseModel):
+    summary: str
